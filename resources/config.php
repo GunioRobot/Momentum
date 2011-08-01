420451543c6da417c1574c11e7a22f86366bbd57
Adrian Ayala <adrian.ayala17@gmail.com>
2011-08-01 14:01:50 -0500

    Archivos de configuracion

---
+++ resources/config.php
@@ -0,0 +1,10 @@
+<?php
+    $config['db_host'] = 'localhost';
+    $config['db_user'] = 'root';
+    $config['db_pass'] = '';
+    $config['db_name'] = 'momentum';
+    
+    foreach ($config as $k => $v) {
+        define(strtoupper($k), $v);
+    }
+?>
